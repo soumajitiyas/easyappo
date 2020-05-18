93622915e05681f22db468d551841384e7b078a5
soumajitiyas <soumajitpaul13@gmail.com>
2020-05-18 17:35:11 +0530

    Data Type Changes

--- src/main/java/com/easyapp/dao/entity/ConfigEntity.java
+++ src/main/java/com/easyapp/dao/entity/ConfigEntity.java
@@ -2,12 +2,15 @@ package com.easyapp.dao.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "tbl_config")
 public class ConfigEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "key")
     private String key;
